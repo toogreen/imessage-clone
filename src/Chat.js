14c53e7f16abb4f2be1ea660369dbb9a78b0625a
David Gagnon <toogreen@15inch.lan>
2020-10-19 23:05:18 -0400

    Sidebar done, now building main chat message window - commiting before MbP dies again on me

---
+++ src/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Chat.css"
+
+function Chat() {
+    return (
+      <div className="chat">
+        <div className="chat__header">
+            <h4>To: <span className="chat__name">Channel name</span></h4>
+            <strong>Details</strong>
+        </div>
+        {/* chat messages */}
+        {/* chat input */}
+      </div>
+    );
+}
+
+export default Chat
